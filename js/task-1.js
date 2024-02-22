e66a87451baef88d64fcd615d6872647887cf365
KamenetskaUla <152808377+KamenetskaUla@users.noreply.github.com>
2024-02-22 21:56:09 +0200

    add

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,7 @@
 function makeTransaction(quantity, pricePerDroid) {
   const result = quantity * pricePerDroid;
-  console.log(`You ordered ${quantity} droids worth ${result} credits!`);
+  return `You ordered ${quantity} droids worth ${result} credits!`;
 }
-makeTransaction(5, 3000);
-makeTransaction(3, 1000);
-makeTransaction(10, 500);
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
